6ee1e9ca56f6db667174cda4ed03411980832605
Maurice .H <66009198+WilderMO@users.noreply.github.com>
2022-05-15 20:13:02 +0200

    release

---
+++ N1_NPCsOff/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'NO-ONE Development'
+description 'Dieses Script schaltet alle AIs aus auf der Welt'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
